d008821f98a55679a5eb97f2c9f44d4bee43160c
Pierre76400 <pierre.leplay@gmail.com>
2015-07-26 12:10:24 +0200

    create project

---
+++ src/main/java/org/pierro/hello/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.pierro.hello;
+
+/**
+ * @author pierro
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world !");
+	}
+
+}
